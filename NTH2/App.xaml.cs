74829b6f230325e6d8362647bf30afdfb70b9b62
kgsensei <jt@kellem.com>
2022-05-14 21:58:12 -0400

    Initial commit

---
+++ NTH2/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WindowsInput.Native;
+using WindowsInput;
+
+namespace NitroType2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+
+    public partial class App : Application
+    {
+        public static bool isCheatRunning = false;
+        public async static void simulateTypingText(string Text, int typingDelay)
+        {
+            isCheatRunning = true;
+            InputSimulator sim = new InputSimulator();
+            char[] letters = Text.Replace("\u00A0", " ").ToCharArray();
+            foreach (char letter in letters)
+            {
+                sim.Keyboard.TextEntry(letter);
+                await Task.Delay(typingDelay);
+            }
+            isCheatRunning = false;
+        }
+    }
+}
---
+++ NTH2/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NitroType2
+{
+    class thingsorwhatever
+    {
+        public static int typingSpeed { get; set; } = 5;
+        public static bool godMode { get; set; } = false;
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (!thingsorwhatever.godMode)
+            {
+                thingsorwhatever.typingSpeed = Convert.ToInt32(e.NewValue);
+            }
+            webview2.Focus();
+        }
+
+        async private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var html = await webview2.ExecuteScriptAsync("z=document.getElementsByClassName('dash-letter');m='';for(let i=0;i<z.length;i++){m=m+z[i].innerText};window.chrome.webview.postMessage(''+m);");
+            webview2.Focus();
+        }
+
+        private void Webview2_WebMessageReceived(object sender, Microsoft.Web.WebView2.Core.CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            webview2.Focus();
+            if (!App.isCheatRunning)
+            {
+                App.simulateTypingText(e.TryGetWebMessageAsString(), thingsorwhatever.typingSpeed);
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (App.isCheatRunning)
+            {
+                webview2.Focus();
+            }
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (!App.isCheatRunning)
+            {
+                if (thingsorwhatever.godMode)
+                {
+                    thingsorwhatever.typingSpeed = Convert.ToInt32(cheatTypeSpeedSlider.Value);
+                }
+                else
+                {
+                    thingsorwhatever.typingSpeed = 0;
+                }
+                thingsorwhatever.godMode = !thingsorwhatever.godMode;
+            }
+            webview2.Focus();
+        }
+    }
+}
