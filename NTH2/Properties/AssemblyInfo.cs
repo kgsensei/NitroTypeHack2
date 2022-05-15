9863fe4c94c53406930ddad62359946c6d2a9152
kgsensei <jt@kellem.com>
2022-05-14 22:03:46 -0400

    Patches.

--- NTH2/Properties/AssemblyInfo.cs
+++ NTH2/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NTH2")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright © kgsensei 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
